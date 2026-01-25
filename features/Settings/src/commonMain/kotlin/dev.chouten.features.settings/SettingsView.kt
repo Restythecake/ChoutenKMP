71c2c84080b6db09b3d6fe10734e4f24cd804d4a
inumakieu <kempcompany05@gmail.com>
2026-01-25 13:11:51 +0100

    Fix unused import breaking compilation

--- features/Settings/src/commonMain/kotlin/dev.chouten.features.settings/SettingsView.kt
+++ features/Settings/src/commonMain/kotlin/dev.chouten.features.settings/SettingsView.kt
@@ -2,7 +2,6 @@ package dev.chouten.features.settings
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SettingsView() {
