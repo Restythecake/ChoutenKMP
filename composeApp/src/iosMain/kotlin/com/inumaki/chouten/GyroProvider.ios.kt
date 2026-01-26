5489c11ac80965d8d7e0519bef2d57e7af0a3cb0
inumakieu <kempcompany05@gmail.com>
2026-01-26 13:41:44 +0100

    Add Gyro logic to ios

--- composeApp/src/iosMain/kotlin/com/inumaki/chouten/GyroProvider.ios.kt
+++ composeApp/src/iosMain/kotlin/com/inumaki/chouten/GyroProvider.ios.kt
@@ -2,16 +2,33 @@ package com.inumaki.chouten
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import platform.CoreMotion.CMMotionManager
+import platform.Foundation.NSOperationQueue
+import kotlin.math.PI
 
-actual class GyroProvider: HeadingSource {
+actual class HeadingProvider {
 
+    private val motionManager = CMMotionManager()
     private val _heading = MutableStateFlow(0f)
-    override val heading: StateFlow<Float> = _heading
+    actual val heading: StateFlow<Float> = _heading
 
-    override fun start() {
-        _heading.value = 0f
+    actual fun start() {
+        if (!motionManager.deviceMotionAvailable) return
+
+        motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+        motionManager.startDeviceMotionUpdatesToQueue(
+            NSOperationQueue.mainQueue()
+        ) { motion, _ ->
+            motion ?: return@startDeviceMotionUpdatesToQueue
+
+            val yawRad = motion.attitude.yaw
+            val yawDeg = (yawRad * 180.0 / PI).toFloat()
+
+            _heading.value = normalizeDegrees(yawDeg)
+        }
     }
 
-    override fun stop() {
+    actual fun stop() {
+        motionManager.stopDeviceMotionUpdates()
     }
 }
